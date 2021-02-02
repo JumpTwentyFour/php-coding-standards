b5c234f7dca71a025d16fb5b56b79d66309b8f8d
Alex Williams <alex@jump24.co.uk>
2021-02-02 07:44:49 +0000

    NEW: Request validation rule + setup of stan.

---
+++ src/Laravel/PHPStan/RequestValidationRule.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace JumpTwentyFour\PhpCodingStandards\Laravel\PHPStan;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Rules\Rule;
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+
+class RequestValidationRule implements Rule
+{
+    /**
+     * @return string
+     */
+    public function getNodeType(): string
+    {
+        return MethodCall::class;
+    }
+
+    /**
+     * @param Node $node
+     * @param Scope $scope
+     * @return string[]
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (! $node->name instanceof Node\Identifier || $node->name->toString() !== 'validate') {
+            return [];
+        }
+
+        if (! $this->isCalledOn($node->var, $scope, Request::class) &&
+            ! $this->isCalledOn($node->var, $scope, Controller::class)) {
+            // Method was not called on a Request or Controller, so no errors.
+            return [];
+        }
+
+        return ["All request validation should be done in the form of a form request "
+            . "https://laravel.com/docs/8.x/validation#form-request-validation and not performed inline in a "
+            . " controller to ensure a separation of concerns."];
+    }
+}
