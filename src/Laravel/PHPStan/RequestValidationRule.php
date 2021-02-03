811e71bf61e3722fd934df18e3f3855daffe75c6
Alex Williams <alex@jump24.co.uk>
2021-02-03 07:50:42 +0000

    UPDATE: Add tests

--- src/Laravel/PHPStan/RequestValidationRule.php
+++ src/Laravel/PHPStan/RequestValidationRule.php
@@ -4,10 +4,12 @@ namespace JumpTwentyFour\PhpCodingStandards\Laravel\PHPStan;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use PhpParser\Node\Expr\MethodCall;
-use PHPStan\Rules\Rule;
 use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
+use PHPStan\Rules\Rule;
+use PHPStan\Type\ObjectType;
 
 class RequestValidationRule implements Rule
 {
@@ -26,18 +28,34 @@ class RequestValidationRule implements Rule
      */
     public function processNode(Node $node, Scope $scope): array
     {
-        if (! $node->name instanceof Node\Identifier || $node->name->toString() !== 'validate') {
+        if (!$node->name instanceof Node\Identifier || $node->name->toString() !== 'validate') {
             return [];
         }
-
-        if (! $this->isCalledOn($node->var, $scope, Request::class) &&
-            ! $this->isCalledOn($node->var, $scope, Controller::class)) {
+        if (
+            !$this->isCalledOn($node->var, $scope, Request::class)
+            && !$this->isCalledOn($node->var, $scope, Controller::class)
+        ) {
             // Method was not called on a Request or Controller, so no errors.
             return [];
         }
 
         return ["All request validation should be done in the form of a form request "
             . "https://laravel.com/docs/8.x/validation#form-request-validation and not performed inline in a "
-            . " controller to ensure a separation of concerns."];
+            . "controller to ensure a separation of concerns."];
+    }
+
+    /**
+     * Determine whether the Expr was called on a class instance.
+     *
+     * @param \PhpParser\Node\Expr $expr
+     * @param \PHPStan\Analyser\Scope $scope
+     * @param string $className
+     * @return bool
+     */
+    protected function isCalledOn(Expr $expr, Scope $scope, string $className)
+    {
+        $calledOnType = $scope->getType($expr);
+
+        return (new ObjectType($className))->isSuperTypeOf($calledOnType)->yes();
     }
 }
