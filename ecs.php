f4f2ee6b253a8260bbb7668422d44ae2741286f5
Alex Williams <alex@jump24.co.uk>
2021-02-09 08:11:33 +0000

    UPDATE: Tweak

--- ecs.php
+++ ecs.php
@@ -39,7 +39,7 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::SETS, [SetList::CLEAN_CODE, SetList::COMMON, SetList::PSR_1, SetList::PSR_12]);
+    $parameters->set(Option::SETS, [SetList::CLEAN_CODE, SetList::PSR_1, SetList::PSR_12]);
 
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
