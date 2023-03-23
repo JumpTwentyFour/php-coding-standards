e71998fc73ba8e2d9b7231e1929d952e8f028674
Mante Balzaraviciute <mante@jump24.co.uk>
2023-03-23 13:28:12 +0000

    ADD: New sniff for final controller

---
+++ src/Sniffs/FinalControllerSniff.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use SlevomatCodingStandard\Helpers\TokenHelper;
+
+class FinalControllerSniff implements Sniff
+{
+    public const CONTROLLER_NOT_FINAL = 'ControllerNotFinal';
+
+    public function register(): array
+    {
+        return [
+            T_CLASS,
+        ];
+    }
+
+    public function process(File $phpcsFile, $classPointer): void
+    {
+        $previousPointer = TokenHelper::findPreviousEffective($phpcsFile, $classPointer - 1);
+
+        $tokens = $phpcsFile->getTokens();
+
+        $className = $phpcsFile->getDeclarationName($classPointer);
+
+        if (str_contains($className, 'Test')) {
+            return;
+        }
+
+        if (str_contains($className, 'Controller') === false) {
+            return;
+        }
+
+        if ($tokens[$previousPointer]['code'] === T_FINAL) {
+            return;
+        }
+
+        $fix = $phpcsFile->addFixableError(
+            'All controllers should be declared using "final" keyword.',
+            $classPointer - 1,
+            self::CONTROLLER_NOT_FINAL
+        );
+
+        if ($fix === false) {
+            return;
+        }
+
+        $phpcsFile->fixer->beginChangeset();
+        $phpcsFile->fixer->addContent($classPointer - 1, 'final ');
+        $phpcsFile->fixer->endChangeset();
+    }
+}
