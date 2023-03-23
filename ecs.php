6f42a44433187b1aa037afed9d7c8dc139b8d8a4
Mante Balzaraviciute <mante@jump24.co.uk>
2023-03-23 11:13:16 +0000

    ADD: camelCase variable name sniff

--- ecs.php
+++ ecs.php
@@ -9,6 +9,7 @@ use PHP_CodeSniffer\Standards\Generic\Sniffs\NamingConventions\CamelCapsFunction
 use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
 use PHP_CodeSniffer\Standards\PSR12\Sniffs\ControlStructures\BooleanOperatorPlacementSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\ControlStructures\ControlSignatureSniff;
+use PHP_CodeSniffer\Standards\Squiz\Sniffs\NamingConventions\ValidVariableNameSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\PHP\CommentedOutCodeSniff;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\Strings\ConcatenationSpacingSniff;
 use PhpCsFixer\Fixer\ArrayNotation\TrailingCommaInMultilineArrayFixer;
@@ -69,6 +70,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         CamelCapsFunctionNameSniff::class => [
             '/tests/**',
         ],
+        'PHP_CodeSniffer\Standards\Squiz\Sniffs\NamingConventions\ValidVariableNameSniff.PrivateNoUnderscore',
+        'PHP_CodeSniffer\Standards\Squiz\Sniffs\NamingConventions\ValidVariableNameSniff.MemberNotCamelCaps',
         'SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff.MissingTraversableTypeHintSpecification',
         'SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff.MissingTraversableTypeHintSpecification',
         'SlevomatCodingStandard\Sniffs\TypeHints\ParameterTypeHintSniff.MissingNativeTypeHint',
@@ -149,4 +152,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(MethodSpacingSniff::class);
     $services->set(BackedEnumTypeSpacingSniff::class);
     $services->set(UnusedFunctionParameterSniff::class);
+    $services->set(ValidVariableNameSniff::class);
 };
